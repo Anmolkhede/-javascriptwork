6ba8e3045930723be68a5891792c5f706e74c307
Anmol Khede <166509168+Anmolkhede@users.noreply.github.com>
2024-05-04 19:46:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.write('hello friends');
+console.log("Hello friend"); 
+document.write ("<br>");
+ 
+
+//using var
+var a = "hi this is anmol khede learning js"
+document.write(a)
+
+
+//var work
+var a = 10, b = 205;
+ if(a<b){
+    var x =a+b;
+   
+
+ };
+console.log(x);
+
+
+
+// function
+function anmol()
+{
+    alert("hello coder this is Anmol ");
+}
+
+document.write ("<br>");
+function khede()
+{
+    print();
+}
+function kp()
+{
+    alert("hello coder this is Anmol ");
+}
+
+
